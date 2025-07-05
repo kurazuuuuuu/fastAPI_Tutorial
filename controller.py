fef78dc6fc2612362ee900b1bfe055b3256acd9a
kurazuuuuuu <110393398+kurazuuuuuu@users.noreply.github.com>
2025-07-05 10:43:47 +0900

    part-2 done

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from starlette.templating import Jinja2Templates  # new
 from starlette.requests import Request
+import db
+from models import User, Task
 
 
 app = FastAPI(
@@ -19,4 +21,13 @@ def index(request: Request):
     return templates.TemplateResponse('index.html',{'request': request})  # new 変更！
 
 def admin(request: Request):
-    return templates.TemplateResponse('admin.html', {'request': request, 'username': 'admin'})
+    # ユーザとタスクを取得
+    # とりあえず今はadminユーザのみ取得
+    user = db.session.query(User).filter(User.username == 'admin').first()
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    db.session.close()
+
+    return templates.TemplateResponse('admin.html',
+                                        {'request': request,
+                                        'user': user,
+                                        'task': task})
